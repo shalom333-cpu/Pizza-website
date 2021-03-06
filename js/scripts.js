d24ff1427531c5cd8d8e145bd58cab7dfa0b9d95
shalom333-cpu <elishaolimbas@gmail.com>
2021-03-06 21:59:06 +0300

    add .click, .hide, .slideDown and console.log to button#checkout

--- js/scripts.js
+++ js/scripts.js
@@ -131,5 +131,13 @@ $(document).ready(function(){
       $("#ordersmade").append('<tr><td id="pizzaname">' + newOrder.name + '</td><td id="pizzasize">' + newOrder.size + '</td><td id="pizzacrust">' + newOrder.crust + '</td><td id="pizzatopping">' + newOrder.topping + '</td><td id="totals">' + newOrder.total + '</td></tr>');
       console.log(newOrder);
     });
+
+    $("button#checkout").click(function(){
+      $("button#checkout").hide();
+      $("button.addPizza").hide();
+      $("button.deliver").slideDown(1000);
+      $("#addedprice").slideDown(1000);
+      console.log("Your total bill is sh. " +checkoutTotal);
+    });
   });
 });
