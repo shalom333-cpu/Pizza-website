2f0f750046dd327b225cac27bfcf4e116bdce08b
shalom333-cpu <elishaolimbas@gmail.com>
2021-03-06 20:24:47 +0300

    add switch statement and console.log

--- js/scripts.js
+++ js/scripts.js
@@ -87,6 +87,41 @@ $(document).ready(function(){
       $.each($("input[name='toppings']:checked"), function () {
         ptopping.push($(this).val());
       });
+      console.log(ptopping.join(", "));
+      switch (psize) {
+        case "0":
+          price = 0;
+          break;
+        case "large":
+          price = 1200;
+          console.log(price);
+          break;
+        case "medium":
+          price = 850;
+          console.log("The price is " + price);
+          break;
+        case "small":
+          price = 600;
+          console.log(price);
+        default:
+          console.log("error");
+      }
+      switch (pcrust) {
+        case "0":
+          crust_price = 0;
+          break;
+        case "Crispy":
+          crust_price = 200;
+          break;
+        case "Stuffed":
+          crust_price = 150;
+          break;
+        case "Gluten-free":
+          crust_price = 180;
+          break;
+        default:
+          console.log("No price");
+      }
     });
   });
 });
