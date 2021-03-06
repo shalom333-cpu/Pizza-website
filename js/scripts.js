cdcf2cf38ab9c1e70b7ba766cd9766c6d444674a
shalom333-cpu <elishaolimbas@gmail.com>
2021-03-06 19:27:37 +0300

    add switch statement and use console.log

--- js/scripts.js
+++ js/scripts.js
@@ -17,5 +17,40 @@ $(document).ready(function(){
       ptopping.push($(this).val());
     });
     console.log(ptopping.join(", "));
+
+    switch (psize) {
+      case "0":
+        price = 0;
+        break;
+      case "large":
+        price = 1200;
+        console.log(price);
+        break;
+      case "medium":
+        price = 850;
+        console.log("The price is " + price);
+        break;
+      case "small":
+        price = 600;
+        console.log(price);
+      default:
+        console.log("error");
+    }
+    switch (pcrust) {
+      case "0":
+        crust_price = 0;
+        break;
+      case "Crispy":
+        crust_price = 200;
+        break;
+      case "Stuffed":
+        crust_price = 250;
+        break;
+      case "Gluten-free":
+        crust_price = 180;
+        break;
+      default:
+        console.log("No price");
+    }
   });
 });
