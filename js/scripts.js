cd016f7c1589a890fd396f2427b908cb3b42f26f
shalom333-cpu <elishaolimbas@gmail.com>
2021-03-06 19:49:56 +0300

    use .join

--- js/scripts.js
+++ js/scripts.js
@@ -73,5 +73,10 @@ $(document).ready(function(){
     let checkoutTotal = 0;
     checkoutTotal = checkoutTotal + total;
 
+    $("#pizzaname").html($(".name option:selected").val());
+    $("#pizzasize").html($("#size option:selected").val());
+    $("#pizzacrust").html($("#crust option:selected").val());
+    $("#pizzatopping").html(ptopping.join(", "));
+    $("#totals").html(total);
   });
 });
